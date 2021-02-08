dc42cc58979270ab752610b11bced2c42d245338
fortuneteller2k <lythe1107@gmail.com>
2021-02-08 20:51:00 +0800

    add horizon colors

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "GTK theme part of the Phocus collection";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in {
+      devShell = import ./shell.nix { inherit pkgs; };
+    }
+  );
+}
